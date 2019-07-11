0d758508e511a30bc653fea05e9534f711d9474c
muhammetssen <enmuhammet23@gmail.com>
2019-07-11 17:08:59 +0300

    Listed post names and publish dates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts = Post.objects.order_by('published_date')
+    return render(request,'blog/post_list.html',{
+        'posts':posts
+
+
+    })
+
+"""def new_post(request):
+    return render
+
+    """
